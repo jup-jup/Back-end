3941d64ce5ea5e17e90f0140c31503eae5571da7
boram0415 <boram04415@naver.com>
2024-07-12 12:33:00 +0900

    로그 파일 및 현재 필요없는 의존성 주석

--- src/main/java/com/jupjup/www/jupjup/controller/MainController.java
+++ src/main/java/com/jupjup/www/jupjup/controller/MainController.java
@@ -1,16 +1,18 @@
 package com.jupjup.www.jupjup.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@Slf4j
 public class MainController {
 
     @GetMapping("/")
     public ResponseEntity<String> index() {
+        log.debug("hello world !");
         return ResponseEntity.status(200).body("ok");
     }
 
