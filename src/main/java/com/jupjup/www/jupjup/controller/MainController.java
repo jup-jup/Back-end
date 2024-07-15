facdd56ab5f2c8200cc1923497b96032089f6a66
boram0415 <boram04415@naver.com>
2024-07-15 23:51:18 +0900

    복구

--- src/main/java/com/jupjup/www/jupjup/controller/MainController.java
+++ src/main/java/com/jupjup/www/jupjup/controller/MainController.java
@@ -12,8 +12,7 @@ public class MainController {
 
     @GetMapping("/")
     public ResponseEntity<String> index() {
-        log.debug("hello world !!!!!");
-        System.out.println("boram branch");
+        log.debug("hello world !");
         return ResponseEntity.status(200).body("ok");
     }
 
