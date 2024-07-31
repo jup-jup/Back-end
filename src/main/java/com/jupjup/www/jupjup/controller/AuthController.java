6c479da9571ee9b5af4c4a0e246a162d9b71da7d
boram0415 <boram04415@naver.com>
2024-07-31 21:56:07 +0900

    스웨거 기본틀  작성완료

--- src/main/java/com/jupjup/www/jupjup/controller/AuthController.java
+++ src/main/java/com/jupjup/www/jupjup/controller/AuthController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 @Slf4j
-public class JwtController {
+public class AuthController {
 
     private final AuthService authService;
 
