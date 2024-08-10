3310af847ab0a0b23a0f517f95c680b7c98de861
choi-yh <yhchoi0102@gmail.com>
2024-08-10 23:38:33 +0900

    feat(giveaway): 나눔 기본 엔티티
    feat/giveaway

---
+++ src/main/java/com/jupjup/www/jupjup/entity/GiveawayEntity.java
@@ -0,0 +1,51 @@
+package com.jupjup.www.jupjup.entity;
+
+import com.jupjup.www.jupjup.enumClass.GiveawayStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Table(name = "giveaway")
+@Getter
+@Entity
+public class GiveawayEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "status")
+    private GiveawayStatus status;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "giver_id")
+    private UserEntity giver;
+
+    // TODO: 이미지의 저장 구현 방법은 고민해볼 것
+    // TODO: cascade & orphanRemoval 에 대한 이해 필요
+    @OneToMany(mappedBy = "giveaway", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<GiveawayImageEntity> imagePaths;
+
+    // TODO: 거래 장소
+
+    // TODO: 댓글 수 -> 관련 채팅 수인가?
+
+    // TODO: 조회 수
+
+}
---
+++ src/main/java/com/jupjup/www/jupjup/entity/GiveawayImageEntity.java
@@ -0,0 +1,27 @@
+package com.jupjup.www.jupjup.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Table(name = "giveaway_image")
+@Getter
+@Entity
+public class GiveawayImageEntity {
+    // TODO: 이미지 저장 방식을 어떤 식으로 구현할 것인지 학습 필요
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "path")
+    private String path; // 이미지 파일이 저장된 경로를 나타낸다.
+
+    private boolean isRepresent; // 대표 이미지
+
+    // @OneToMany 단방향을 써서 부모 엔티티가 주인이 되기보다 양방향 연관관계를 이용해 자식 엔티티가 FK를 관리하는 것이 권장된다고 함.
+    @ManyToOne
+    @JoinColumn(name = "giveaway_id")
+    private GiveawayEntity giveaway;
+
+}
