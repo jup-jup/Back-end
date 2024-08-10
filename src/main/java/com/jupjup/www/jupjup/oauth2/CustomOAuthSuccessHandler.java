63c24b07b442943228cfbef763dce02174db9fb2
boram0415 <boram04415@naver.com>
2024-08-10 09:00:42 +0900

    소설 최종

--- src/main/java/com/jupjup/www/jupjup/oauth2/CustomOAuthSuccessHandler.java
+++ src/main/java/com/jupjup/www/jupjup/oauth2/CustomOAuthSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.jupjup.www.jupjup.oauth2;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jupjup.www.jupjup.entity.RefreshEntity;
 import com.jupjup.www.jupjup.jwt.JWTUtil;
 import com.jupjup.www.jupjup.repository.RefreshTokenRepository;
@@ -9,6 +10,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -16,10 +19,9 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.sql.Ref;
-import java.util.Collection;
-import java.util.Date;
-import java.util.Iterator;
+import java.util.*;
 
 /*로그인 성공 시 호출 페이지*/
 @Component
@@ -50,21 +52,36 @@ public class CustomOAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         GrantedAuthority auth = iterator.next();
 
         String role = auth.getAuthority();
+        log.info("Role : {}", role);
 
         // jwt 발급
         String accessToken = JWTUtil.generateAccessToken(username, "ROLE_USER");
-        String RefreshToken = JWTUtil.generateRefreshToken(username, "ROLE_USER");
+        String refreshToken = JWTUtil.generateRefreshToken(username, "ROLE_USER");
 
         // 리프레시 저장
         refreshTokenRepository.save(RefreshEntity.builder()
-                .refresh(RefreshToken)
+                .refresh(refreshToken)
                 .userEmail(userEmail)
-                .expiration(JWTUtil.RefreshTokenExTimeCul(RefreshToken))
+                .expiration(JWTUtil.RefreshTokenExTimeCul(refreshToken))
                 .build());
 
-        response.setHeader("Authorization", "Bearer " +accessToken);
-        response.addHeader("Set-Cookie", JWTUtil.createCookie(RefreshToken).toString());
-        response.sendRedirect("http://localhost:8080/");
+
+        // HttpHeaders 객체 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+        headers.add(HttpHeaders.SET_COOKIE, JWTUtil.createCookie(refreshToken).toString());
+
+        // JSON 응답 생성
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("username", username);
+        responseBody.put("userEmail", userEmail);
+        String jsonResponse = objectMapper.writeValueAsString(responseBody);
+
+        // 응답 작성
+        response.setContentType("application/json;charset=UTF-8"); // 한글 인코딩 꼭 작성해줘야 함
+        response.getWriter().write(jsonResponse);
+
 
     }
 
