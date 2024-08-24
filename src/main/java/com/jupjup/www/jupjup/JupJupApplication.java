344a37fe4a590176705afa2ed1ebd8bc074a895d
boram0415 <boram04415@naver.com>
2024-08-24 14:24:31 +0900

    file added

--- src/main/java/com/jupjup/www/jupjup/JupJupApplication.java
+++ src/main/java/com/jupjup/www/jupjup/JupJupApplication.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class JupJupApplication {
 
     public static void main(String[] args) {
+        System.out.println("MYSQL_URL: " + System.getenv("MYSQL_URL"));
+        System.out.println("MYSQL_USER: " + System.getenv("MYSQL_USER"));
         SpringApplication.run(JupJupApplication.class, args);
 
     }
