d517ec6041b8cef731302dd2b02a7ea9902048dd
choi-yh <yhchoi0102@gmail.com>
2024-08-26 13:37:32 +0900

    docs(chat): 채팅 & 채팅방 swagger
    feat/swagger

--- src/main/java/com/jupjup/www/jupjup/controller/ChatController.java
+++ src/main/java/com/jupjup/www/jupjup/controller/ChatController.java
@@ -5,12 +5,22 @@ import com.jupjup.www.jupjup.model.dto.chat.ChatList;
 import com.jupjup.www.jupjup.model.dto.chat.CreateChatRequest;
 import com.jupjup.www.jupjup.service.chat.ChatService;
 import com.jupjup.www.jupjup.service.oauth.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Array;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springdoc.core.converters.models.PageableAsQueryParam;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -20,6 +30,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.List;
 
+@Tag(name = "Chat", description = "채팅 API")
 @RequiredArgsConstructor
 @RestController
 // TODO: 전체 서버 관점에서 /api/v1 prefix 를 사용한다면 application.yml 에서 설정하는 것이 좋아보임
@@ -28,6 +39,12 @@ public class ChatController {
 
     private final ChatService chatService;
 
+    @Operation(summary = "create chat", description = "채팅 생성 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "채팅 정상 생성 완료",
+                    headers = @Header(name = HttpHeaders.LOCATION, description = "해당 채팅 url")),
+            @ApiResponse(responseCode = "401", description = "잘못된 유저입니다.")
+    })
     @PostMapping("")
     public ResponseEntity<?> createChat(
             @PathVariable Long roomId,
@@ -50,18 +67,34 @@ public class ChatController {
         }
     }
 
+    @Operation(summary = "get chat list", description = "채팅방의 채팅 리스트 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = ChatList.class)))
+                    }),
+            @ApiResponse(responseCode = "401", description = "잘못된 유저입니다. / 해당 채팅방의 유저가 아닙니다.")
+    })
     @GetMapping("")
-    public ResponseEntity<List<ChatList>> getChatList(
+    public ResponseEntity<?> getChatList(
             @PathVariable Long roomId,
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             Authentication authentication
     ) {
-        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-        List<ChatList> list = chatService.chatList(pageable, roomId, customUserDetails.getUserEmail());
+        try {
+            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+            List<ChatList> list = chatService.chatList(pageable, roomId, customUserDetails.getUserEmail());
 
-        return ResponseEntity
-                .ok()
-                .body(list);
+            return ResponseEntity
+                    .ok()
+                    .body(list);
+
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(e.getMessage());
+        }
     }
 
 }
--- src/main/java/com/jupjup/www/jupjup/controller/ChatRoomController.java
+++ src/main/java/com/jupjup/www/jupjup/controller/ChatRoomController.java
@@ -6,7 +6,17 @@ import com.jupjup.www.jupjup.model.dto.chatRoom.RoomListResponse;
 import com.jupjup.www.jupjup.model.dto.chatRoom.RoomResponse;
 import com.jupjup.www.jupjup.service.chat.RoomService;
 import com.jupjup.www.jupjup.service.oauth.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Array;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -14,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Chat Room", description = "채팅방 API")
 @RequiredArgsConstructor
 @RestController
 // TODO: 전체 서버 관점에서 /api/v1 prefix 를 사용한다면 application.yml 에서 설정하는 것이 좋아보임
@@ -22,6 +33,13 @@ public class ChatRoomController {
 
     private final RoomService roomService;
 
+    @Operation(summary = "create room", description = "채팅방 생성 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CreateRoomResponse.class))),
+            @ApiResponse(responseCode = "401", description = "잘못된 유저입니다.")
+    })
     @PostMapping("")
     public ResponseEntity<?> createRoom(@RequestBody CreateRoomRequest request, Authentication authentication) {
 
@@ -39,7 +57,13 @@ public class ChatRoomController {
         }
     }
 
-    // 해당 유저의 채팅 목록 전체를 가져옵니다.
+    @Operation(summary = "get rooms", description = "채팅방 목록 API. 해당 유저의 채팅 목록 전체를 가져옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = RoomListResponse.class)))),
+            @ApiResponse(responseCode = "401", description = "잘못된 유저입니다.")
+    })
     @GetMapping("")
     public ResponseEntity<?> getRooms(Authentication authentication) {
         try {
@@ -57,7 +81,12 @@ public class ChatRoomController {
         }
     }
 
-    // 참여 중인 채팅방 디테일을 가져옵니다.
+    @Operation(summary = "get rooms", description = "채팅방 정보 API. 참여 중인 채팅방 디테일을 가져옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = @Content(schema = @Schema(implementation = RoomResponse.class))),
+            @ApiResponse(responseCode = "401", description = "잘못된 유저입니다.")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<RoomResponse> getRoom(@PathVariable Long id) {
 
