15f865a54ae677a069f5c8eb5131e235c37e3819
boram0415 <boram04415@naver.com>
2024-08-30 07:41:12 +0900

    test코드 주석처리

--- src/test/java/com/jupjup/www/jupjup/chat/repository/RoomRepositoryCustomImplTest.java
+++ src/test/java/com/jupjup/www/jupjup/chat/repository/RoomRepositoryCustomImplTest.java
@@ -1,105 +1,105 @@
-package com.jupjup.www.jupjup.chat.repository;
-
-import com.jupjup.www.jupjup.chat.entity.QRoom;
-import com.jupjup.www.jupjup.chat.entity.QUserChatRoom;
-import com.jupjup.www.jupjup.chat.entity.Room;
-import com.jupjup.www.jupjup.domain.entity.User;
-import com.jupjup.www.jupjup.domain.entity.giveaway.Giveaway;
-import com.jupjup.www.jupjup.domain.repository.GiveawayRepository;
-import com.jupjup.www.jupjup.domain.repository.UserRepository;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-@SpringBootTest
-@Transactional
-public class RoomRepositoryCustomImplTest {
-
-    @Autowired
-    private final JPAQueryFactory queryFactory;
-
-    @Autowired
-    private final RoomRepository roomRepository;
-
-    @Autowired
-    private final GiveawayRepository giveawayRepository;
-
-    @Autowired
-    private final UserRepository userRepository;
-
-
-    @Autowired
-    public RoomRepositoryCustomImplTest(JPAQueryFactory queryFactory, RoomRepository roomRepository, GiveawayRepository giveawayRepository, UserRepository userRepository) {
-        this.queryFactory = queryFactory;
-        this.roomRepository = roomRepository;
-        this.giveawayRepository = giveawayRepository;
-        this.userRepository = userRepository;
-    }
-
-    @DisplayName("채팅방이 없는 경우 새로운 채팅방 생성")
-    @Test
-    public void findByUserIdAndGiveawayId() {
-        // given. 데이터 세팅
-
-        // 유저 저장
-        User user1 = User.builder()
-                .providerKey("providerKey")
-                .username("user1")
-                .userEmail("userEmail")
-                .role("role")
-                .build();
-
-        User user2 = User.builder()
-                .providerKey("providerKey")
-                .username("user2")
-                .userEmail("userEmail")
-                .role("role")
-                .build();
-
-        userRepository.saveAll(List.of(user1, user2));
-
-        // 나눔 정보 저장
-        Giveaway giveaway = Giveaway.builder()
-                .title("title")
-                .giverId(user1.getId())
-                .build();
-        giveawayRepository.save(giveaway);
-
-        // 채팅방 생성
-        Room room = Room.builder()
-                .giveawayId(giveaway.getId())
-                .build();
-
-        // user_chat_room 정보 추가
-        room.addUserChatRoom(user1.getId());
-        room.addUserChatRoom(user2.getId());
-
-        roomRepository.save(room);
-
-
-        final QUserChatRoom qUserChatRoom = QUserChatRoom.userChatRoom;
-        final QRoom qRoom = QRoom.room;
-
-        // when. 쿼리
-        List<Room> rooms = queryFactory
-                .select(qRoom)
-                .from(qRoom)
-                .join(qRoom.userChatRooms, qUserChatRoom)
-                .where(
-                        qUserChatRoom.user.id.eq(user1.getId())
-                                .and(qRoom.giveawayId.eq(giveaway.getId()))
-                )
-                .fetch();
-
-        // then. 검증
-        Assertions.assertEquals(rooms.size(), 1); // giveaway_id & user_id 에 채팅방은 1개
-        Assertions.assertEquals(rooms.get(0), room);
-    }
-
-}
+//package com.jupjup.www.jupjup.chat.repository;
+//
+//import com.jupjup.www.jupjup.chat.entity.QRoom;
+//import com.jupjup.www.jupjup.chat.entity.QUserChatRoom;
+//import com.jupjup.www.jupjup.chat.entity.Room;
+//import com.jupjup.www.jupjup.domain.entity.User;
+//import com.jupjup.www.jupjup.domain.entity.giveaway.Giveaway;
+//import com.jupjup.www.jupjup.domain.repository.GiveawayRepository;
+//import com.jupjup.www.jupjup.domain.repository.UserRepository;
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import jakarta.transaction.Transactional;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//
+//@SpringBootTest
+//@Transactional
+//public class RoomRepositoryCustomImplTest {
+//
+//    @Autowired
+//    private final JPAQueryFactory queryFactory;
+//
+//    @Autowired
+//    private final RoomRepository roomRepository;
+//
+//    @Autowired
+//    private final GiveawayRepository giveawayRepository;
+//
+//    @Autowired
+//    private final UserRepository userRepository;
+//
+//
+//    @Autowired
+//    public RoomRepositoryCustomImplTest(JPAQueryFactory queryFactory, RoomRepository roomRepository, GiveawayRepository giveawayRepository, UserRepository userRepository) {
+//        this.queryFactory = queryFactory;
+//        this.roomRepository = roomRepository;
+//        this.giveawayRepository = giveawayRepository;
+//        this.userRepository = userRepository;
+//    }
+//
+//    @DisplayName("채팅방이 없는 경우 새로운 채팅방 생성")
+//    @Test
+//    public void findByUserIdAndGiveawayId() {
+//        // given. 데이터 세팅
+//
+//        // 유저 저장
+//        User user1 = User.builder()
+//                .providerKey("providerKey")
+//                .username("user1")
+//                .userEmail("userEmail")
+//                .role("role")
+//                .build();
+//
+//        User user2 = User.builder()
+//                .providerKey("providerKey")
+//                .username("user2")
+//                .userEmail("userEmail")
+//                .role("role")
+//                .build();
+//
+//        userRepository.saveAll(List.of(user1, user2));
+//
+//        // 나눔 정보 저장
+//        Giveaway giveaway = Giveaway.builder()
+//                .title("title")
+//                .giverId(user1.getId())
+//                .build();
+//        giveawayRepository.save(giveaway);
+//
+//        // 채팅방 생성
+//        Room room = Room.builder()
+//                .giveawayId(giveaway.getId())
+//                .build();
+//
+//        // user_chat_room 정보 추가
+//        room.addUserChatRoom(user1.getId());
+//        room.addUserChatRoom(user2.getId());
+//
+//        roomRepository.save(room);
+//
+//
+//        final QUserChatRoom qUserChatRoom = QUserChatRoom.userChatRoom;
+//        final QRoom qRoom = QRoom.room;
+//
+//        // when. 쿼리
+//        List<Room> rooms = queryFactory
+//                .select(qRoom)
+//                .from(qRoom)
+//                .join(qRoom.userChatRooms, qUserChatRoom)
+//                .where(
+//                        qUserChatRoom.user.id.eq(user1.getId())
+//                                .and(qRoom.giveawayId.eq(giveaway.getId()))
+//                )
+//                .fetch();
+//
+//        // then. 검증
+//        Assertions.assertEquals(rooms.size(), 1); // giveaway_id & user_id 에 채팅방은 1개
+//        Assertions.assertEquals(rooms.get(0), room);
+//    }
+//
+//}
--- src/test/java/com/jupjup/www/jupjup/chat/service/RoomServiceTest.java
+++ src/test/java/com/jupjup/www/jupjup/chat/service/RoomServiceTest.java
@@ -1,30 +1,30 @@
-package com.jupjup.www.jupjup.chat.service;
-
-import com.jupjup.www.jupjup.chat.repository.RoomRepository;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@Transactional
-public class RoomServiceTest {
-
-    @Autowired
-    private final RoomRepository roomRepository;
-
-    public RoomServiceTest(RoomRepository roomRepository) {
-        this.roomRepository = roomRepository;
-    }
-
-    @Test
-    public void create() {
-        // given. 데이터 세팅
-
-        // when. 생성 수행
-
-        // then. 검증
-
-    }
-
-}
+//package com.jupjup.www.jupjup.chat.service;
+//
+//import com.jupjup.www.jupjup.chat.repository.RoomRepository;
+//import jakarta.transaction.Transactional;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//@Transactional
+//public class RoomServiceTest {
+//
+//    @Autowired
+//    private final RoomRepository roomRepository;
+//
+//    public RoomServiceTest(RoomRepository roomRepository) {
+//        this.roomRepository = roomRepository;
+//    }
+//
+//    @Test
+//    public void create() {
+//        // given. 데이터 세팅
+//
+//        // when. 생성 수행
+//
+//        // then. 검증
+//
+//    }
+//
+//}
