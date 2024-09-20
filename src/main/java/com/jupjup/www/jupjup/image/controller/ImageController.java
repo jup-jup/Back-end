11a4f570c039f3de0f51ffd3d0d60bb6663b8d34
choi-yh <yhchoi0102@gmail.com>
2024-09-20 15:54:20 +0900

    refactor(image): GlobalExceptionHandler 적용
    refactor/exception

--- src/main/java/com/jupjup/www/jupjup/image/controller/ImageController.java
+++ src/main/java/com/jupjup/www/jupjup/image/controller/ImageController.java
@@ -35,7 +35,6 @@ import java.util.List;
 public class ImageController {
 
     private final ImageService imageService;
-    private static final String BEARER_PREFIX = "Bearer ";
 
     @Operation(summary = "upload image", description = "이미지 업로드 API")
     @ApiResponses(value = {
@@ -46,21 +45,11 @@ public class ImageController {
             @ApiResponse(responseCode = "500", description = "파일 업로드 에러")
     })
     @PostMapping("")
-    public ResponseEntity<?> upload(@RequestParam("files") List<MultipartFile> files, @Valid @RequestHeader("Authorization") String accessToken) {
+    public List<UploadImageResponse> upload(@RequestParam("files") List<MultipartFile> files, @Valid @RequestHeader("Authorization") String accessToken) throws IOException {
         // TODO: authorization header 에서 userId 뽑아오는 방법이 이게 최선일까..
         // boram : JWTUtil 에 별도로 빼서 중복코드 없애봤어요 !
         Long userId = JWTUtil.parseUserIdFromToken(accessToken);
-
-        try {
-            List<UploadImageResponse> images = imageService.save(files, userId);
-            return ResponseEntity
-                    .ok()
-                    .body(images);
-        } catch (IOException e) {
-            return ResponseEntity
-                    .internalServerError()
-                    .body(e.getMessage());
-        }
+        return imageService.save(files, userId);
     }
 
     @Operation(summary = "get image", description = "이미지 메타 정보를 가져오는 API")
@@ -72,17 +61,8 @@ public class ImageController {
             @ApiResponse(responseCode = "400", description = "존재하지 않는 id 입니다.")
     })
     @GetMapping("/{id}")
-    public ResponseEntity<?> getImage(@PathVariable Long id) {
-        try {
-            GetImageResponse resp = imageService.find(id);
-            return ResponseEntity
-                    .ok()
-                    .body(resp);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(e.getMessage());
-        }
+    public GetImageResponse getImage(@PathVariable Long id) {
+        return imageService.find(id);
     }
 
     @Operation(summary = "display image", description = "실제 이미지를 보여주기 위한 API")
@@ -99,24 +79,13 @@ public class ImageController {
             @ApiResponse(responseCode = "500", description = "서버에 파일이 없는 경우")
     })
     @GetMapping("/display/{id}")
-    public ResponseEntity<?> showImage(@PathVariable Long id) {
-        try {
-            DisplayImageDTO dto = imageService.display(id);
+    public ResponseEntity<?> showImage(@PathVariable Long id) throws IOException {
+        DisplayImageDTO dto = imageService.display(id);
 
-            return ResponseEntity.ok()
-                    .contentType(MediaType.parseMediaType(dto.getContentType()))
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + dto.getEncodedFileName() + "\"")
-                    .body(dto.getResource());
-        } catch (IOException e) {
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(e.getMessage());
-        } catch (IllegalArgumentException e) {
-            // TODO: 값이 없는 경우 200으로 처리할지 400으로 처리할지, 404로 처리할지..
-            return ResponseEntity
-                    .badRequest()
-                    .body(e.getMessage());
-        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(dto.getContentType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + dto.getEncodedFileName() + "\"")
+                .body(dto.getResource());
     }
 
 }
