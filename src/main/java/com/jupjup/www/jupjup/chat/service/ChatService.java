00b4f3985a0a7fe02068f5f83b09a969645068a4
choi-yh <yhchoi0102@gmail.com>
2024-09-20 17:58:41 +0900

    refactor(chat): custom exception 적용
    refactor/exception

--- src/main/java/com/jupjup/www/jupjup/chat/service/ChatService.java
+++ src/main/java/com/jupjup/www/jupjup/chat/service/ChatService.java
@@ -4,6 +4,8 @@ import com.jupjup.www.jupjup.chat.dto.chat.ChatDTO;
 import com.jupjup.www.jupjup.chat.entity.Chat;
 import com.jupjup.www.jupjup.chat.repository.ChatRepository;
 import com.jupjup.www.jupjup.chat.repository.RoomRepository;
+import com.jupjup.www.jupjup.common.exception.CustomException;
+import com.jupjup.www.jupjup.common.exception.ErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -31,7 +33,7 @@ public class ChatService {
     public List<ChatDTO> chatList(Pageable pageable, Long roomId, Long userId) {
         // 해당 room 에 참여중인 user 인지 확인
         roomRepository.findByIdAndUserId(roomId, userId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 채팅방의 유저가 아닙니다."));
+                .orElseThrow(() -> new CustomException(ErrorCode.NO_PERMISSION));
 
         // 해당 룸의 채팅 호출. TODO: 시간 순 정렬 필요
         List<Chat> chatList = chatRepository.findAllByRoomId(pageable, roomId);
